5ae330520d77af45f71b17a34515165061d93f8b
Kirill <nordwestcode@gmail.com>
2016-10-17 19:02:30 +0200

    19

--- 19/src/com/company/Main.java
+++ 19/src/com/company/Main.java
@@ -2,7 +2,18 @@ package com.company;
 
 public class Main {
 
+    // Algorithm, complexity - O(n)
+    static int bitCount(int num){
+        int counter = 0;
+        while (num!= 0){
+            // If the last bit equal to 1, counter increase
+            if ((num & 1) == 1) counter++;
+            num = num >>> 1;
+        }
+        return counter;
+        }
     public static void main(String[] args) {
-	// write your code here
+	int num = 414748364; //00011000101110001000111011001100 /  n=14
+        System.out.println(bitCount(num));
     }
 }
