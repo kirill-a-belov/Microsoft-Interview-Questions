a5c9f06d30393151ff0191ca198d1eba81817e5c
Kirill <nordwestcode@gmail.com>
2016-10-18 14:00:33 +0200

    20.
    L1 -> L2 - > L3 ->.... ->Ln
    
    Before loop:
    1. Mark L2 as current (enter to the loop)
    2. Mark L1 (first) as a previous
    3. Erase L1 direction (it became the last one and point nothing)
    
    In loop (complexity - O(n):
    1. Enter in loop with L2
    2. Save L3 to temp
    3. Redirect L2 to L1
    4. Mark L2 as previous element (moving a "cart")
    5. Mark L3 as current element
    Go to 1.
    
    After loop (Ln is the last one):
    1. Redirect Ln to Ln-1 (prev)
    2. Mark Ln as a first element of the list

---
+++ 20/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    LinkedList list = new LinkedList();
+
+        list.add("Hello");
+        list.add("world");
+        list.add("and");
+        list.add("let");
+        list.add("the");
+        list.add("force");
+        list.add("be");
+        list.add("with");
+        list.add("you!");
+
+        list.print();
+        list.reverse();
+        list.print();
+    }
+
+
+
+}
